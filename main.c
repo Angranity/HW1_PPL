22440ed334348075b619679600a4085796289d53
Angranity <angranity1998@gmail.com>
2021-03-12 02:44:48 +0200

    ex 5

--- main.c
+++ main.c
@@ -1,7 +1,33 @@
 #include <stdio.h>
 #include "q3.h"
+#include "q5.h"
 
 void main()
 {
-	q3();
+	//q3();
+	//int arr[] = { 34, 12, 24, 65, 63};
+	//int arraysize = (sizeof(arr) / sizeof(*arr));
+	//unsigned char buf[101] = { 0 };
+
+	//for (int k = 0; k < arraysize; k++) {
+	//	buf[arr[k]]++;
+	//}
+
+	//char i = 0;
+	//for (int k = 0; k <= 6; k++) {
+	//	arr[i] = k;
+	//	i += buf[k];
+	//}
+
+	//for (int j = 0; j < arraysize; j++)
+	//{
+	//	printf("%d  ", arr[j]);
+	//}
+	q5_A();
+	puts("");
+	q5_B();
+	puts("");
+	q5_C();
+	puts("");
+	q5_E();
 }
---
+++ q5.h
@@ -0,0 +1,11 @@
+#pragma once
+#define SIZE 5
+void swap(int *a, int *b);
+int* sortArray_A(int arr[], int size);
+int* sortArray_B(int arr[], int size);
+int* sortArray_C(int arr[]);
+int* sortArray_E(int arr[], int size);
+void q5_A();
+void q5_C();
+void q5_B();
+void q5_E();
